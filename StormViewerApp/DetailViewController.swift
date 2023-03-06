3ea63a140dfeaa1c4c6eaa4bad2cdc34d2457341
Catharina Balliego <catharina_@hotmail.com.br>
2023-03-06 12:32:10 -0300

    display image with imageController and ViewController

---
+++ StormViewerApp/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  StormViewerApp
+//
+//  Created by Catharina Balliego on 06/03/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+
+
+}
