857b1757829a8a144e90ebb46277858048a7189a
Catharina Balliego <catharina_@hotmail.com.br>
2023-03-22 14:36:24 -0300

    load selected image

--- StormViewerApp/DetailViewController.swift
+++ StormViewerApp/DetailViewController.swift
@@ -7,18 +7,23 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
+class DetailViewController: UIViewController{
     var selectedImage: String?
+    var imageView = UIImageView()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         title = selectedImage
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         navigationItem.largeTitleDisplayMode = .never
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+        imageView.contentMode = .scaleToFill
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+        view.addSubview(imageView)
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
